e77701a030e5760730f790f355b9f554d9ceaf44
Conrad Nied <aconrad@fb.com>
2022-09-29 22:51:49 +0200

    Add image displaying functions

--- changelog.md
+++ changelog.md
@@ -2,3 +2,8 @@
 
 2022-09-26
 * Added initial data files & an inital notebook loading the data.
+  * Examples in notebook 01_Initial_Loading_Examples
+
+2022-09-29
+* Added image displaying functions, copied from older environment
+  * Examples in notebook 01_Initial_Loading_Examples
---
+++ src/image.py
@@ -0,0 +1,163 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+
+class RasterImage():
+    def __init__ (self, imageinfo=[], map_transforms=None,
+                  dataset=None, region=None, resolution=None, folder=None, 
+                  n_neighbors=None, n_rows=None, n_cols = None):
+        # imageinfo = object with dataname, regionname, layername, n_neighbors, resolution
+        # optional: n_rows, n_cols, folder
+        # transforms = MapTransforms
+        
+        # Names
+        self.folder = folder if folder else (imageinfo['folder'] if 'folder' in imageinfo else 'Figs/')
+        self.dataset = dataset if dataset else (imageinfo['dataset'] if 'dataset' in imageinfo else '')
+        self.resolution = resolution if resolution else (imageinfo['resolution'] if 'resolution' in imageinfo else '')
+        self.region = region if region else (imageinfo['region'] if 'region' in imageinfo else '')
+        
+        # Dimension
+        self.n_rows = n_rows if n_rows else (imageinfo['n_rows'] if 'n_rows' in imageinfo else 0)
+        self.n_cols = n_cols if n_cols else (imageinfo['n_cols'] if 'n_cols' in imageinfo else 0)
+        self.n_nodes = self.n_rows * self.n_cols
+        self.n_neighbors = n_neighbors if n_neighbors else (imageinfo['n_neighbors'] if 'n_neighbors' in imageinfo else 0)
+        
+        # Other
+        self.map_transforms = map_transforms
+         
+        # Initialize empty data
+        self.nodes_colors = np.ones([self.n_nodes,1], dtype=float) * np.array([0, 0, 0, 1]).reshape([1,4])
+        self.layer_names = []
+        self.fig = None
+        self.background = None
+        
+    def setData(self, data):
+        self.data = data
+        return self
+    
+    def addLayer(self, name, values, nodes_selected=None, 
+                 combine='set', transforms=[], colormap=None,
+                 color_channel=None, opacity=None, dissolve=None):
+        self.addToFilename(name)
+        
+        # values is expected to be 1 int, 3 length array (3 colors), n_nodes length array or 2D array that's n_nodes x 4
+        #   factor: float
+        #   color: 3/4 length array
+        #    
+        value_format = 'unknown'
+        n_nodes_provided = 1
+        n_color_channels = 3 # or 4
+        if (isinstance(values, (int, float))):
+            value_format = 'factor'
+        else:
+            values = np.array(values)
+            shape = values.shape
+            n_dims = len(shape)
+            if(n_dims == 1 and shape[0] <= 4):
+                value_format = 'color'
+                n_color_channels = shape[0]
+            elif(n_dims == 1):
+                value_format = 'nodes_factor'
+                n_nodes_provided = shape[0]
+            else: # n_dims = 2
+                value_format = 'nodes_color'
+                n_nodes_provided = shape[0]
+                n_color_channels = shape[1]
+                
+        # Nodes Colored / Mask processing
+        # Mask should be either n_nodes bool array or array of nodes_index that's colored
+        n_nodes_selected = self.n_nodes
+        if (nodes_selected is None): # If not specified, change all nodes
+            nodes_selected = np.arange(self.n_nodes)
+        else:
+            if(nodes_selected.dtype is np.dtype('bool')):
+                n_nodes_selected = np.count_nonzero(nodes_selected)
+            else:
+                n_nodes_selected = len(nodes_selected)
+            
+            # If the values also has the dimension of n_nodes, but we are coloring less, extract the colored ones
+            if (n_nodes_provided != 1 and n_nodes_provided != n_nodes_selected):
+                assert(n_nodes_provided == self.n_nodes)
+                values = values[nodes_selected]
+
+        # Transforms
+        if ('border' in transforms):
+            assert (value_format == 'nodes_factor')
+            values = self.map_transforms.getNodesBorder(values)
+        if ('prandom' in transforms):
+            values = values * 1619 % 251
+        if ('norm' in transforms): # puts them in range 0 to 1
+            values = plt.Normalize()(values)
+        if (colormap is not None): # Apply Color
+            assert (value_format == 'nodes_factor')
+            
+            if (colormap == 'prism'):
+                values = plt.cm.prism(values)
+            elif (colormap == 'diverge'): # diverge
+                values = plt.cm.RdYlBu(values)
+            elif (colormap == 'qual'): # qualitiative
+                values = plt.cm.Paired(values)
+            else: # rainbow
+                values = plt.cm.rainbow(values)
+            value_format = 'nodes_color'
+            n_color_channels = 4
+            
+        # Expand the values to full color (nodes x channels) or keep as single number
+        if (value_format == 'color'):
+            values = values.reshape([1, n_color_channels]).repeat(n_nodes_selected, axis=0)
+        elif (value_format == 'nodes_factor' and color_channel is None):
+            values = values.reshape([n_nodes_selected, 1]).repeat(n_color_channels, axis=1)
+        else: # factor (returns 1) or nodes_color returns (n_nodes_colored x n_color_channels)
+            values = values
+        
+        # Apply to pixel colors
+        if (color_channel is not None): # 0 to 3: red, green, blue, opacity
+            self.nodes_colors[nodes_selected, color_channel] = values
+        elif (combine == 'set'): # value doesn't matter
+            self.nodes_colors[nodes_selected, :n_color_channels] = values
+        elif (combine == 'add'): # factor [0-1] used in formula original * (1 - value) + new * value
+            #self.nodes_colors = self.nodes_colors * (1 - factor) + data * factor
+            opacity = opacity if opacity is not None else 0.5 # make custom
+            dissolve = dissolve if dissolve is not None else 1 - opacity
+            self.nodes_colors[nodes_selected, :n_color_channels] *= dissolve
+            self.nodes_colors[nodes_selected, :n_color_channels] += values * opacity
+        elif (combine == 'multiply'):
+            self.nodes_colors[nodes_selected, :n_color_channels] *= values
+        else:
+            raise Exception('Invalid way to combine, must set color_channel [0-3] or set combine [set, add, multiply]')
+            
+        # Ensures no node is too big or too small
+        self.nodes_colors = np.fmax(np.fmin(self.nodes_colors, 1), 0)
+        return self
+    
+    def addToFilename(self, name=''):
+        if(name is not None and name != ''):
+            self.layer_names.append(name)
+        return self    
+    
+    def getFilename(self):
+        n_nei = 'nei' + str(self.n_neighbors) if self.n_neighbors > 0 else ''
+        layers = '-'.join(self.layer_names)
+        file_attributes = [self.dataset, self.resolution, self.region, n_nei, layers]
+        file_attributes = filter(lambda x: x is not None and x != '', file_attributes)
+        
+        filename = '_'.join(file_attributes) + '.png'
+        self.filename = self.folder + filename
+        return filename
+    
+    def save(self, save_fig = False):
+        filename = self.getFilename()
+        img.imsave(self.filename, self.nodes_colors.reshape([self.n_rows, self.n_cols, 4]))
+        if(save_fig and self.fig is not None):
+            self.fig.savefig(self.folder + 'fig_' + filename, bbox_inches='tight')
+        return self
+        
+    def display(self):
+        self.fig = plt.figure(figsize=(12,8))
+        plt.imshow(self.nodes_colors.reshape([self.n_rows, self.n_cols, 4]))
+        plt.show()
+        return self
+    
+    def final(self):
+        # So output of self doesn't appear if you don't want it
+        return
